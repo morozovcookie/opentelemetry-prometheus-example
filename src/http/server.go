c22df07d29455ef3735e6279fcb9ef52ecaca63d
Konstantin Morozov <morozov.cookie@gmail.com>
2022-07-01 12:28:01 +0300

    http: server

--- src/http/server.go
+++ src/http/server.go
@@ -1,1 +1,83 @@
 package http
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+// A Server defines parameters for running an HTTP server.
+type Server struct {
+	addr string
+
+	readTimeout       time.Duration
+	readHeaderTimeout time.Duration
+	writeTimeout      time.Duration
+	idleTimeout       time.Duration
+	maxHeaderBytes    int
+
+	instance *http.Server
+}
+
+// NewServer returns a new Server instance.
+func NewServer(addr string, handler http.Handler, opts ...ServerOptionFunc) *Server {
+	srv := &Server{
+		addr: addr,
+
+		readTimeout:       DefaultReadTimeout,
+		readHeaderTimeout: DefaultReadHeaderTimeout,
+		writeTimeout:      DefaultWriteTimeout,
+		idleTimeout:       DefaultIdleTimeout,
+		maxHeaderBytes:    DefaultMaxHeaderBytes,
+
+		instance: nil,
+	}
+
+	for _, opt := range opts {
+		opt(srv)
+	}
+
+	srv.instance = &http.Server{
+		Addr:              addr,
+		Handler:           handler,
+		TLSConfig:         nil,
+		ReadTimeout:       srv.readTimeout,
+		ReadHeaderTimeout: srv.readHeaderTimeout,
+		WriteTimeout:      srv.writeTimeout,
+		IdleTimeout:       srv.idleTimeout,
+		MaxHeaderBytes:    srv.maxHeaderBytes,
+		TLSNextProto:      nil,
+		ConnState:         nil,
+		ErrorLog:          nil,
+		BaseContext:       nil,
+		ConnContext:       nil,
+	}
+
+	return srv
+}
+
+// Address returns a served server address.
+func (srv *Server) Address() string {
+	return srv.addr
+}
+
+// ListenAndServe listens on the TCP network address srv.Addr and then calls Serve to handle requests on incoming
+// connections. Accepted connections are configured to enable TCP keep-alives.
+func (srv *Server) ListenAndServe() error {
+	if err := srv.instance.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+		return fmt.Errorf("start http server: %w", err)
+	}
+
+	return nil
+}
+
+// Shutdown gracefully shuts down the server without interrupting any active connections.
+func (srv *Server) Shutdown(ctx context.Context) error {
+	if err := srv.instance.Shutdown(ctx); err != nil {
+		return fmt.Errorf("shutdown http server: %w", err)
+	}
+
+	return nil
+}
---
+++ src/http/server_option.go
@@ -0,0 +1,61 @@
+package http
+
+import (
+	"net/http"
+	"time"
+)
+
+// ServerOptionFunc is the option for configure Server object.
+type ServerOptionFunc func(server *Server)
+
+// DefaultReadTimeout is the default duration for reading the entire request, including the body.
+const DefaultReadTimeout = time.Millisecond * 100
+
+// WithReadTimeout sets up the maximum duration for reading the entire request, including the body.
+func WithReadTimeout(timeout time.Duration) ServerOptionFunc {
+	return func(server *Server) {
+		server.readTimeout = timeout
+	}
+}
+
+// DefaultReadHeaderTimeout is the default amount of time allowed to read request headers.
+const DefaultReadHeaderTimeout = time.Millisecond * 100
+
+// WithReadHeaderTimeout sets up the amount of time allowed reading request headers.
+func WithReadHeaderTimeout(timeout time.Duration) ServerOptionFunc {
+	return func(server *Server) {
+		server.readHeaderTimeout = timeout
+	}
+}
+
+// DefaultWriteTimeout is the default duration before timing out writes of the response.
+const DefaultWriteTimeout = time.Millisecond * 100
+
+// WithWriteTimeout set the maximum duration before timing out writes of the response.
+func WithWriteTimeout(timeout time.Duration) ServerOptionFunc {
+	return func(server *Server) {
+		server.writeTimeout = timeout
+	}
+}
+
+// DefaultIdleTimeout is the default amount of time to wait for the next request when keep-alives are enabled.
+const DefaultIdleTimeout = time.Millisecond * 100
+
+// WithIdleTimeout set the maximum amount of time to wait for the next request when keep-alives are enabled.
+func WithIdleTimeout(timeout time.Duration) ServerOptionFunc {
+	return func(server *Server) {
+		server.idleTimeout = timeout
+	}
+}
+
+// DefaultMaxHeaderBytes controls the default number of bytes the server will read parsing the request header's keys and
+// values, including the request line.
+const DefaultMaxHeaderBytes = http.DefaultMaxHeaderBytes
+
+// WithMaxHeaderBytes set the maximum number of bytes the server will read parsing the request header's keys and values,
+// including the request line.
+func WithMaxHeaderBytes(bytes int) ServerOptionFunc {
+	return func(server *Server) {
+		server.maxHeaderBytes = bytes
+	}
+}
